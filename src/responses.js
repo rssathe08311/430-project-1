992e392d61c3e59de118e92514079a2ea3a668d7
Reva Sathe <rss9622@g.rit.edu>
2024-10-03 19:18:36 -0400

    more work done

--- src/responses.js
+++ src/responses.js
@@ -3,6 +3,9 @@ const fs = require('fs');
 const index = fs.readFileSync(`${__dirname}/../client/client.html`);
 const style = fs.readFileSync(`${__dirname}/../client/style.css`);
 
+
+
+//retrieves html page
 const getIndex = (request, response) => {
     response.writeHead(200, { 'Content-Type': 'text/html' });
   
@@ -13,10 +16,75 @@ const getIndex = (request, response) => {
     response.end();
 };
 
+//retrieves css file
 const getCSS = (request, response) => {
     response.writeHead(200, { 'Content-Type': 'text/css' });
     if (request.method !== 'HEAD') {
       response.write(style);
     }
     response.end();
+};
+
+const respondJSON = (request, response, status, object) => {
+  const content = JSON.stringify(object);
+  response.writeHead(status, {
+    'Content-Type': 'application/json',
+    'Content-Length': Buffer.byteLength(content, 'utf8'),
+  });
+
+  if(request.method !== 'HEAD' && status !== 204) {
+    response.write(content);
+  }
+  response.end();
+}
+
+const notFound = (request, response) => {
+  const responseJSON = {
+    message: 'The page you are looking for was not found',
+    id: 'notFound',
+  };
+  return respondJSON(request, response, 404, responseJSON);
+}
+
+//GET Requests
+
+const getBooks = (request, response) => {
+
+};
+
+const getAuthor = (request, response) => {
+
+};
+
+const getTitle = (request, response) => {
+
+};
+
+const getReviews = (request, response) => {
+
+};
+
+
+//POST Requests
+const addBook = (request, response) => {
+
+};
+
+const addReview = (request, response) => {
+
+};
+
+
+
+module.exports = {
+  getIndex,
+  getCSS,
+  getBooks,
+  getAuthor,
+  getTitle,
+  getReviews,
+  addBook,
+  addReview,
+  notFound,
+  badRequest,
 };
--- src/server.js
+++ src/server.js
@@ -1,10 +1,67 @@
 const http = require('http');
+const fs = require('fs');
 const responseHandler = require('./responses.js');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
+let booksJson = [];
+
+//load books.json to booksJson
+fs.readFile(`${__dirname}/../data/books.json`, 'utf8', (err, data => {
+  if(err){
+    console.error('Error loading data: ', err);
+    return;
+  }
+  booksJson = JSON.parse(data);
+}))
 
 
+const handleGetHead = (request, response, parsedUrl) => {
+  if (parsedUrl.pathname === '/') {
+    responseHandler.getIndex(request, response);
+  } else if (parsedUrl.pathname === '/style.css') {
+    responseHandler.getCSS(request, response);
+  } else if (parsedUrl.pathname === '/getBooks') {
+    responseHandler.getBooks(request, response, parsedUrl);
+  } else if (parsedUrl.pathname === '/getAuthor') {
+    responseHandler.getAuthor(request, response, parsedUrl);
+  } 
+  else if (parsedUrl.pathname === '/getTitle') {
+    responseHandler.getTitle(request, response, parsedUrl);
+  }
+  else if (parsedUrl.pathname === '/getReviews') {
+    responseHandler.getReviews(request, response, parsedUrl);
+  }
+  else {
+    responseHandler.notFound(request, response);
+  }
+};
+
+const handlePost = (request, response, parsedUrl) => {
+  if(parsedUrl.pathname === '/addBook'){
+    parseBody(request, response, responseHandler.addBook);
+  }
+  else if(parsedUrl.pathname === '/addReview'){
+    parseBody(request, response, responseHandler.addReview);
+  }
+  else {
+    responseHandler.notFound(request, response);
+  }
+}
+
+
+const onRequest = (request, response) => {
+  const protocol = request.connection.encrypted ? 'https' : 'http';
+  const parsedUrl = new URL(request.url, `${protocol}://${request.headers.host}`);
+
+  if (request.method === 'POST') {
+    handlePost(request, response, parsedUrl);
+  } else if (['GET', 'HEAD'].includes(request.method)) {
+    handleGetHead(request, response, parsedUrl);
+  } else {
+    responseHandler.notFound(request, response);
+  }
+}
 
 http.createServer(onRequest).listen(port, () => {
     console.log(`Listening on 127.0.0.1: ${port}`);
